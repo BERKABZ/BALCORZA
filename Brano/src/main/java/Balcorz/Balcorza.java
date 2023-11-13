878023d3e200c3259937a3018831a425005fc5af
BERKABZ <150333064+BERKABZ@users.noreply.github.com>
2023-11-13 15:29:59 +0800

    Add files via upload

---
+++ Brano/src/main/java/Balcorz/Balcorza.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Balcorz;
+
+/**
+ *
+ * @author WS
+ */
+public class Balcorza {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
